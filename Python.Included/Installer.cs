33298cdf7bc8fec868ec5e1a5b9a274c33c53f15
epignatelli <eduardo.pignatelli@burohappold.com>
2019-11-28 16:00:38 +0000

    make sure we delete the ._pth file

--- Python.Included/Installer.cs
+++ Python.Included/Installer.cs
@@ -56,8 +56,6 @@ namespace Python.Included
                     // allow pip on embedded python installation
                     // see https://github.com/pypa/pip/issues/4207#issuecomment-281236055
                     var pth = Path.Combine(EmbeddedPythonHome, PYTHON_VERSION + "._pth");
-                    //string lines = File.ReadAllText(pth);
-                    //lines = lines.Replace("#import site", "import site");
                     File.Delete(pth);
                 }
                 catch { }
