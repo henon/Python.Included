e6844afaf170b5dd47815e2fce72786b237fb10a
Meinrad Recheis <meinrad.recheis@gmail.com>
2020-07-23 08:46:02 +0200

    Added NetCoreApp example

---
+++ examples/NetCoreExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Python.Included;
+using Python.Runtime;
+
+namespace NetCoreExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Installer.SetupPython();
+            PythonEngine.Initialize();
+            dynamic sys=PythonEngine.ImportModule("sys");
+            Console.WriteLine("Python version: " + sys.version);
+        }
+    }
+}
