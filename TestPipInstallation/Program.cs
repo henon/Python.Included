0403c10835689d5535b20265f0663b48185c576d
Ons CHERIF <ocherif@mantu.com>
2020-08-03 10:30:24 +0100

    Added required packages to the test project

--- TestPipInstallation/Program.cs
+++ TestPipInstallation/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Python.Included;
+using Python.Runtime;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace TestPipInstallation
 {
